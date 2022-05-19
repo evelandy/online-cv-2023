19506ebb4875217bc355ae04a2f92fb86e085698
William Griffin <40611326+evelandy@users.noreply.github.com>
2022-05-19 01:34:32 -0500

    Added Dark and Light Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React, { FC, useEffect } from 'react';
+import React, { ChangeEventHandler, FC, useEffect } from 'react';
 import { TopNav } from './components/Nav';
 import { BrowserRouter, Route, Switch, RouteComponentProps  } from 'react-router-dom';
 import routes from './config/routes';
 import './components/styles/appStyles.css';
+import './components/styles/darkMode.css';
 
 
 const App = () => {
+  const storedTheme = localStorage.getItem("theme");
+
+  const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+
+  const defaultDark = storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+  const toggleTheme: ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.checked) {
+      setDark();
+    } else {
+      setLight();
+    }
+  }
+  
   useEffect(() => {
     console.log('Loading App')
   }, [])
@@ -31,6 +55,24 @@ const App = () => {
             )
           })}
         </Switch>
+        <div className="toggle-theme-wrapper mt-14 mb-16">
+          <span>☀️</span>
+          <label className="toggle-theme" htmlFor="checkbox">
+            <input
+              type="checkbox"
+              id="checkbox"
+              onChange={toggleTheme}
+              defaultChecked={defaultDark}
+            />
+            <div className="slider round"></div>
+          </label>
+          <span>🌒</span>
+        </div>
+        {/* <button onClick={() => {
+              localStorage.getItem("theme") === "dark" ? setLight() : setDark();
+          }} className='border-2 rounded p-2'>
+            Change Mode
+        </button> */}
       </BrowserRouter>
     </div>
   );
